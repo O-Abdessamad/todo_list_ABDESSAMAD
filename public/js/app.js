46ffe63512f76239b792915bda7e132fd3f8ada3
Abdessamad <abdessamadouali04@gmail.com>
2023-06-09 13:38:42 +0100

    creaction des variables

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+// les variable
+let input_text = document.querySelector(".input_text");
+let btn_add = document.querySelector(".btn_add");
+let emplacement = document.querySelector(".emplacement");
+
+btn_add.addEventListener("click",(element)=>{
+    
+})
