530239e305908da3cd531ee8f6cc2e9be7c34366
Abdessamad <abdessamadouali04@gmail.com>
2023-06-09 15:08:05 +0100

    cration des element end

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,36 @@ console.log(emplacement);
 btn_add.addEventListener("click",(element)=>{
 
     // cration des element
-   
+    if (input_text.value != "") {
+        element.preventDefault();
+        //creation de h3
+        let h3 = document.createElement("h3");
+        emplacement.appendChild(h3);
+        h3.setAttribute("class", "h3_dsplay")
+        //creation de p
+        let p = document.createElement("p");
+        p.textContent = input_text.value;
+        h3.appendChild(p);
+        //creation de icon
+        let icon = document.createElement("i");
+        icon.innerHTML = '<i class="fa-solid verif fa-check" value="done" style="color: #42f312;"></i> <i class="fa-solid update fa-pen-to-square" ></i>';
+        
+        h3.appendChild(icon);
+        //creation de X
+        let spane = document.createElement("span");
+        spane.innerHTML = "x";
+        h3.appendChild(spane);
+    }
+    // suprition des elements
+    let X = document.querySelectorAll("span");
+    
+    for (let i = 0; i < X.length; i++) {
+        X[i].addEventListener("click", ()=>{
+            X[i].parentNode.remove();
+            
+        })
+    }
+    //
+    input_text.value = "";
 })
 
