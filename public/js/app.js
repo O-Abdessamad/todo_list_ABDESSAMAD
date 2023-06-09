bccd4136a127d411cc0ffd6167de5f305c178237
Abdessamad <abdessamadouali04@gmail.com>
2023-06-09 15:06:48 +0100

    creation des element

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,12 @@
 // les variable
 let input_text = document.querySelector(".input_text");
 let btn_add = document.querySelector(".btn_add");
-let emplacement = document.querySelector(".emplacement");
+let emplacement = document.querySelector(".emplacement ");
+console.log(emplacement);
 
 btn_add.addEventListener("click",(element)=>{
-    
+
+    // cration des element
+   
 })
+
